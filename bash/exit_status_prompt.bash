9be9cddb6e1e1ceb086be1ec8789d7c165272631
Koki Fukuda <ko.fu.dev@gmail.com>
2020-08-05 20:45:33 +0900

    Improve

--- bash/exit_status_prompt.bash
+++ bash/exit_status_prompt.bash
@@ -1,29 +1,27 @@
-__last_exit="$?"
+# Saves $? first to avoid being overritten by other commands.
+_prompt_last_exit="$?"
+function __exit_status_prompt() {
+    local message signal
 
-if [ "$__last_exit" -eq 0 ]; then
-    echo -en '\e[38;5;14m'
-    __message='success'
-else
-    echo -en '\e[38;5;13m'
-    __message="failure ($__last_exit)"
-fi
-
-# Although some exceptions exist, exit status greater than 128 indicates
-# signaled termination of the process, and its signal is (status code) - 128.
-if [ "$__last_exit" -gt 128 ]; then
-    __signum="$((__last_exit-128))"
-    # `kill -l SIGNUM` prints signal name, like INT or TERM.
-    __signal="$(kill -l "$__signum" 2>/dev/null)"
-
-    if [ -z "$__signal" ]; then
-        __signal='unknown signal'
+    if [ "$_prompt_last_exit" -eq 0 ]; then
+        echo -en '\e[38;5;14m'
+        message='success'
     else
-        __signal="SIG$__signal"
+        echo -en '\e[38;5;13m'
+        message="failure ($_prompt_last_exit)"
     fi
-    __message="failure ($__last_exit/$__signal)"
-fi
 
-echo -n "//=> $__message"
-echo -e '\e[0m'
+    # Although some exceptions exist, exit status greater than 128 indicates
+    # signaled termination of the process, and its signal is (status code) - 128.
+    if [ "$_prompt_last_exit" -gt 128 ]; then
+        # `kill -l SIGNUM` prints signal name, like INT or TERM.
+        signal="$(kill -l "$((_prompt_last_exit-128))" 2>/dev/null)"
 
-unset __last_exit __message
+        message="failure ($_prompt_last_exit/${signal:-unknown signal})"
+    fi
+
+    echo -n "//=> $message"
+    echo -e '\e[0m'
+} && __exit_status_prompt; unset __exit_status_prompt
+
+unset _prompt_last_exit
