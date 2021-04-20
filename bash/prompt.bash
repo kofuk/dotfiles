564138d20352ef68bc40d4da8a1be01f42c9007e
Koki Fukuda <ko.fu.dev@gmail.com>
2021-04-20 17:44:28 +0900

    Improve

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -3,47 +3,45 @@ function __construct_prompt() {
     # color escape sequences
     local color_reset color_green color_blue color_ssh_tag
 
-    case "$TERM" in
+    case "${TERM}" in
         xterm-color|*-256color) color_prompt=yes;;
     esac
 
-    if [[ "$color_prompt" = 'yes' ]]; then
+    if [[ "${color_prompt}" = 'yes' ]]; then
         color_reset='\[\033[0m\]'
         color_green='\[\033[1;32m\]'
         color_blue='\[\033[1;34m\]'
-        color_ssh_tag='\[\033[1;37;44m\]'
+        color_tag='\[\033[1;37;44m\]'
     fi
 
-    if [ ! -z "$SSH_CLIENT" ]; then
-        _PS1="$color_ssh_tag SSH $color_reset "
-        host='@\h'
-    else
-        local tag_name=
-        if command -v systemd-detect-virt &>/dev/null; then
-            case "$(systemd-detect-virt)" in
-                none) ;;
-                systemd-nspawn)
-                    tag_name='NSPAWN'
-                    ;;
-            esac
-        fi
+    local tag_name=
+    if [ ! -z "${SSH_CLIENT}" ]; then
+        tag_name='SSH'
+    elif command -v systemd-detect-virt &>/dev/null; then
+        case "$(systemd-detect-virt)" in
+            none) ;;
+            systemd-nspawn)
+                tag_name='NSPAWN'
+                ;;
+        esac
+    fi
 
-        if [ -z "${tag_name}" ]; then
-            _PS1=
-        else
-            _PS1="$color_ssh_tag ${tag_name} $color_reset "
-        fi
+    if [ -z "${tag_name}" ]; then
+        _PS1=
+    else
+        _PS1="${color_tag} ${tag_name} ${color_reset} "
+        host='@\h'
     fi
 
     # Placeholder for exit status
-    _PS1="$_PS1@@"
+    _PS1="${_PS1}@@"
 
-    _PS1="$_PS1$color_green$user$host$color_reset:$color_blue$cwd$color_reset\\\$ "
+    _PS1="${_PS1}${color_green}${user}${host}${color_reset}:${color_blue}${cwd}${color_reset}\\\$ "
 
     # If this is an xterm set the title to user@host:dir
-    case "$TERM" in
+    case "${TERM}" in
         xterm*|rxvt*)
-            _PS1="$_PS1\[\033]0;\u$h: \w\a\]"
+            _PS1="${_PS1}\[\033]0;\u${h}: \w\a\]"
             ;;
         *)
             ;;
@@ -52,7 +50,7 @@ function __construct_prompt() {
     # PROMPT_COMMAND sets PS1
 
     # show exit status of previous command.
-    PROMPT_COMMAND="$(cat "$_DOTFILES_BASH_ROOT/exit_status_prompt.bash")"
+    PROMPT_COMMAND="$(cat "${_DOTFILES_BASH_ROOT}/exit_status_prompt.bash")"
 } && __construct_prompt; unset __construct_prompt
 
 PROMPT_DIRTRIM=5
