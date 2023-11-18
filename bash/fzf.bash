50bf56459b9e80415ee051d7376d747447876481
Koki Fukuda <ko.fu.dev@gmail.com>
2023-11-18 11:47:16 +0900

    Improve

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -1,7 +1,15 @@
+export FZF_DEFAULT_OPTS='--height 40%'
+
 __K_history() {
-    local output
-    READLINE_LINE="$(builtin fc -lnr -10000000 | sed 's/^\t //' | fzf --height 40% --bind=ctrl-z:ignore)"
+    READLINE_LINE="$(builtin fc -lnr -10000000 | sed 's/^\t //' | fzf --bind=ctrl-z:ignore)"
     READLINE_POINT=0x7fffffff
 }
+bind -x '"\C-r": __K_history'
 
-bind -x '"\C-r": "__K_history"'
+__K_complete_file() {
+    local output
+    output="$(fzf -m | sed -E "s/'/'\\\\''/; s/^.*$/'&'/" | tr \\n ' ')"
+    READLINE_LINE="${READLINE_LINE:0:READLINE_POINT}${output}${READLINE_LINE:READLINE_POINT}"
+    (( READLINE_POINT += ${#output} ))
+}
+bind -x '"\C-y": __K_complete_file'
